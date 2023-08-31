fa100a0e6053139fa74c55cefd6ff140e6f693aa
ChennXIao <61040179+ChennXIao@users.noreply.github.com>
2023-08-31 15:38:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ app.config['JSONIFY_MIMETYPE'] = "application/json;charset=utf-8"
 def index():
 	cnt = mysql.connector.connect(**db_config)
 	cur = cnt.cursor(dictionary=True,buffered=True)
-	api = "SHOW COLUMNS FROM Attraction;"
+	api = "Select * from attraction where id =2;"
 	print(api)
 	cur.execute(api)
 	result = cur.fetchall()
