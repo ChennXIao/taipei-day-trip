95d0d911d3b09aa3b6221016c16ab33855881562
ChennXIao <61040179+ChennXIao@users.noreply.github.com>
2023-09-09 14:46:50 +0800

    Update template.js

--- static/template.js
+++ static/template.js
@@ -203,6 +203,19 @@ function scroll(){
      });
 
 }
+const debouncedFetchData = debounce(fetch_data, 500);
+
+
+function debounce(func, delay) {
+  let timer;
+  return function () {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(this, arguments);
+    }, delay);
+  };
+}
+
 
 
 
